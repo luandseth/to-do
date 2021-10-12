d4427e760245588471993924cafb705ae761d3ce
= <=>
2021-10-12 04:53:47 -0300

    Created a todo app, only left to add styles

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import TodoList from './components/TodoList/index.js'
+
+function App() {
+  return (
+    <div className="App">
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
